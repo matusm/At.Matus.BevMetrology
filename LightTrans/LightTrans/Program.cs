ec30e1ed2e2300a3d7ac2385af231e91c0b89b75
Michael Matus <michael@matus.at>
2024-03-23 15:43:06 +0100

    Simple test app LightTrans

--- LightTrans/LightTrans/Program.cs
+++ LightTrans/LightTrans/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Globalization;
 using At.Matus.BevMetrology;
+using At.Matus.StatisticPod;
 
 namespace LightTrans
 {
@@ -7,7 +9,7 @@ namespace LightTrans
     {
         static int Main(string[] args)
         {
-
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
 
             #region filename logic
             if (args.Length==0)
@@ -19,18 +21,40 @@ namespace LightTrans
             #endregion
 
             SpectralQuantity spectralTransmission = SpectralQuantity.LoadFromCsv(filename);
-            
-            Console.WriteLine($"File {spectralTransmission.Name} with {spectralTransmission.NumberOfValues} values.");
-            Console.WriteLine($"Wavelength range {spectralTransmission.MinWavelength} nm to {spectralTransmission.MaxWavelength} nm.");
+
+            Console.WriteLine();
+            Console.WriteLine($"File '{spectralTransmission.Name}' with {spectralTransmission.NumberOfValues} values.");
+            Console.WriteLine($"Wavelength range: {spectralTransmission.MinWavelength} nm to {spectralTransmission.MaxWavelength} nm.");
             Console.WriteLine();
 
             double z = BevCie.Integrate(spectralTransmission.GetValueFor, BevCie.CieIlluminantA, BevCie.CieV);
             double n = BevCie.Integrate(BevCie.CieIlluminantA, BevCie.CieV);
             double lightTransmission = z / n;
 
-            Console.WriteLine($"Light transmission factor: {lightTransmission}");
+            Console.WriteLine($"Light transmission factor (Illuminant A, V_lambda): {lightTransmission:F4} %");
             Console.WriteLine();
-            
+
+            #region Pretty useless Monte Carlo simulation
+            double uFixed = 0.15;
+            int nTries = 100;
+            StatisticPod sp = new StatisticPod();
+            SpectralQuantity specMC;
+            Console.WriteLine("Pretty useless Monte Carlo simulation");
+            for (int i = 0; i <= nTries; i++)
+            {
+                specMC = spectralTransmission.Randomize(uFixed, 0.0);
+                if (i % 10 == 0)
+                    Console.Write("|");
+                else
+                    Console.Write("-");
+                double zMC = BevCie.Integrate(specMC.GetValueFor, BevCie.CieIlluminantA, BevCie.CieV);
+                sp.Update(zMC / n);
+            }
+            Console.WriteLine();
+            Console.WriteLine($"MC (u_fixed = {uFixed:F2} %) : {sp.AverageValue:F4} ({2*sp.StandardDeviation:F4}) %");
+            Console.WriteLine();
+            #endregion
+
             return 0;
         }
     }
